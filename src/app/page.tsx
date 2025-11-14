0836e8b9f6d4f581e21a4c73d234bc255d7527c7
joseph <joseph@gmail.com>
2025-11-15 04:06:22 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black">
+      <h1 className="text-white">hello there</h1>
+    </div>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-white shadow px-6 py-4 flex items-center justify-between fixed">
+      <Link href="/" className="text-xl font-semibold">
+        Syntory
+      </Link>
+      <div className="flex items-center gap-6">
+        <Link href="/login" className="hover:text-blue-500">
+          Login
+        </Link>
+        <Link href="/register" className="hover:text-blue-500">
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  );
+}
