6d2a3bcc3529cf2f63dd10d2970cc1398e968518
otep <josephdaing@gmail.com>
2025-11-25 06:33:23 +0800

    console log added

--- src/app/account/[id]/page.tsx
+++ src/app/account/[id]/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+import { getUser, User } from "@/lib/api/auth";
+
 export default function AccountPage() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const data = await getUser();
+        // console.log("User details:", data); 
+        setUser(data); 
+      } catch (err) {
+        console.warn("Failed to fetch user:", err);
+      }
+    };
+
+    fetchUser(); 
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center font-sans bg-amber-700">
-      <h1 className="text-white">Account ko to</h1>
+      <h1 className="text-white">
+        {user ? `Welcome ${user.name}` : "Loading..."}
+      </h1>
     </div>
   );
 }
