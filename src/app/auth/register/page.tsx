1b45208508580b934f18d03acef74ed9fbd2e24a
otep <josephdaing@gmail.com>
2025-11-27 07:41:00 +0800

    register padding top

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "./registerForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans bg-white p-4">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-white pt-14">
       <RegisterForm />
     </div>
   );
