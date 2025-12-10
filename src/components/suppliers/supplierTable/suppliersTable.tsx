8ff28a4a1bad989bb07b61531fd69c2031f2c68b
otep <josephdaing@gmail.com>
2025-12-11 07:09:44 +0800

    refactor pagination and supplier table

--- src/components/suppliers/supplierTable/suppliersTable.tsx
+++ src/components/suppliers/supplierTable/suppliersTable.tsx
@@ -10,16 +10,8 @@ import SearchAndActions from "./searchAndActions";
 import SupplierTable from "./supplierTable";
 import SupplierCard from "./supplierCard";
 import { Package } from "lucide-react";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
 import { Checkbox } from "@/components/ui/checkbox";
+import SuppliersPagination from "./supplierPagination";
 
 interface SuppliersTableProps {
   suppliers: Supplier[];
@@ -336,85 +328,13 @@ export default function SuppliersTable({
           )}
         </div>
 
+        {/* Use the new pagination component */}
         {!showAll && totalPages > 1 && (
-          <div className="mt-6 flex justify-center">
-            <Pagination>
-              <PaginationContent>
-                <PaginationItem>
-                  <PaginationPrevious
-                    onClick={() =>
-                      currentPage > 1 && handlePageChange(currentPage - 1)
-                    }
-                    className={
-                      currentPage === 1
-                        ? "pointer-events-none opacity-50 "
-                        : "cursor-pointer"
-                    }
-                  />
-                </PaginationItem>
-
-                <PaginationItem>
-                  <PaginationLink
-                    onClick={() => handlePageChange(1)}
-                    isActive={currentPage === 1}
-                    className="cursor-pointer bg-transparent hover:bg-slate-400"
-                  >
-                    1
-                  </PaginationLink>
-                </PaginationItem>
-
-                {currentPage > 3 && (
-                  <PaginationItem>
-                    <PaginationEllipsis />
-                  </PaginationItem>
-                )}
-
-                {currentPage !== 1 && currentPage !== totalPages && (
-                  <PaginationItem>
-                    <PaginationLink
-                      onClick={() => handlePageChange(currentPage)}
-                      isActive={true}
-                      className="cursor-pointer bg-transparent"
-                    >
-                      {currentPage}
-                    </PaginationLink>
-                  </PaginationItem>
-                )}
-
-                {currentPage < totalPages - 2 && (
-                  <PaginationItem>
-                    <PaginationEllipsis />
-                  </PaginationItem>
-                )}
-
-                {totalPages > 1 && (
-                  <PaginationItem>
-                    <PaginationLink
-                      onClick={() => handlePageChange(totalPages)}
-                      isActive={currentPage === totalPages}
-                      className="cursor-pointer bg-transparent hover:bg-slate-400"
-                    >
-                      {totalPages}
-                    </PaginationLink>
-                  </PaginationItem>
-                )}
-
-                <PaginationItem>
-                  <PaginationNext
-                    onClick={() =>
-                      currentPage < totalPages &&
-                      handlePageChange(currentPage + 1)
-                    }
-                    className={
-                      currentPage === totalPages
-                        ? "pointer-events-none opacity-50"
-                        : "cursor-pointer"
-                    }
-                  />
-                </PaginationItem>
-              </PaginationContent>
-            </Pagination>
-          </div>
+          <SuppliersPagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
         )}
       </div>
 
